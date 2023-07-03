565a64bfc56b4146ef4c349edd0345a7dbff66c0
PinkR1ver <3180102330@zju.edu.cn>
2023-07-03 16:35:03 +0800

    Add solution to 2280, slope+rational

---
+++ Leetcode/2280. Minimum Lines to Represent a Line Chart/brute-force.py
@@ -0,0 +1,30 @@
+import numpy as np
+from fractions import Fraction
+
+class Solution:
+    def minimumLines(self, stockPrices: list[list[int]]) -> int:
+
+        if len(stockPrices) <= 1:
+            return 0
+
+        count = 0
+
+        stockPrices.sort(key=lambda x: x[0])
+        slopes = [None] * (len(stockPrices) - 1)
+
+        for i in range(len(stockPrices) - 1):
+            numerator = stockPrices[i + 1][1] - stockPrices[i][1]
+            denominator = stockPrices[i + 1][0] - stockPrices[i][0]
+            slopes[i] = Fraction(numerator, denominator)
+
+        for i in range(len(slopes) - 1):
+            if slopes[i + 1] != slopes[i]:
+                count += 1
+
+        return count + 1
+    
+if __name__ == '__main__':
+    s = Solution()
+    stockPrices = [[1,1],[500000000,499999999],[1000000000,999999998]]
+    print(s.minimumLines(stockPrices))
+
---
+++ Leetcode/2280. Minimum Lines to Represent a Line Chart/readme.md
@@ -0,0 +1,3 @@
+# Brute force
+连续计算斜率，判断断点; 
+在面对高精度时，不使用float类型来解，使用Fraction库保存rational类。保证高精度
