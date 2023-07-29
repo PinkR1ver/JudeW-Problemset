5034c05c35aebe66599e39409a816549ab82aa0e
PinkR1ver <3180102330@zju.edu.cn>
2023-07-30 02:58:22 +0800

    fix error

--- Leetcode/problem_solve_list.py
+++ Leetcode/problem_solve_list.py
@@ -10,5 +10,5 @@ dir_list.pop(dir_list.index('problem_solve_list.py'))
 dir_list.sort(key=lambda x: int(re.search(r'(\d+)', x).group()))
 
 for dir_name in dir_list:
-    print(f"* [{dir_name}](https://github.com/PinkR1ver/JudeW-Problemset/blob/Leetcode/" + dir_name.replace(' ', '%20') + ")")
+    print(f"* [{dir_name}](https://github.com/PinkR1ver/JudeW-Problemset/tree/master/Leetcode/" + dir_name.replace(' ', '%20') + ")")
         
