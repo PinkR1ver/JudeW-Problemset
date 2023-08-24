70ca5b7c54a2dd022460f20bf522f0ab5c6b1fdf
PinkR1ver <3180102330@zju.edu.cn>
2023-08-24 18:43:35 +0800

    init with -1 to save space and time

--- Leetcode/2718. Sum of Matrix After Queries/inverser_update.py
+++ Leetcode/2718. Sum of Matrix After Queries/inverser_update.py
@@ -8,8 +8,8 @@ class Solution(object):
         :rtype: int
         """
         
-        row = np.zeros((n,2))
-        col = np.zeros((n,2))
+        row = np.full(n, -1)
+        col = np.full(n, -1)
         row_count = 0
         col_count = 0
         ans = 0
@@ -19,16 +19,14 @@ class Solution(object):
                 break
             
             if queries[i][0] == 0:
-                if row[queries[i][1]][1] == 0:
-                    row[queries[i][1]][0] = queries[i][2]
-                    row[queries[i][1]][1] = 1
+                if row[queries[i][1]] == -1:
+                    row[queries[i][1]] = queries[i][2]
                     ans += queries[i][2] * (n - row_count)
                     col_count += 1
             
             if queries[i][0] == 1:
-                if col[queries[i][1]][1] == 0:
-                    col[queries[i][1]][0] = queries[i][2]
-                    col[queries[i][1]][1] = 1
+                if col[queries[i][1]] == -1:
+                    col[queries[i][1]] = queries[i][2]
                     ans += queries[i][2] * (n - col_count)
                     row_count += 1
         
