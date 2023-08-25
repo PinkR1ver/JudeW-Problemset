25a9febba03421863137867de245a9d513bb6c1d
PinkR1ver <3180102330@zju.edu.cn>
2023-08-25 14:34:40 +0800

    Make problem solve list print to .txt file

--- Leetcode/problem_solve_list.py
+++ Leetcode/problem_solve_list.py
@@ -1,5 +1,6 @@
 import os
 import re
+import sys
 
 base_path = os.path.dirname(os.path.abspath(__file__))
 #print(base_path)
@@ -9,6 +10,16 @@ dir_list = os.listdir(base_path)
 dir_list.pop(dir_list.index('problem_solve_list.py'))
 dir_list.sort(key=lambda x: int(re.search(r'(\d+)', x).group()))
 
-for dir_name in dir_list:
-    print(f"* [{dir_name}](https://github.com/PinkR1ver/JudeW-Problemset/tree/master/Leetcode/" + dir_name.replace(' ', '%20') + ")")
+
+# Open the file in write mode
+with open("output.txt", "w") as file:
+    # Redirect the standard output to the file
+    original_stdout = sys.stdout
+    sys.stdout = file
+    
+    for dir_name in dir_list:
+        print(f"* [{dir_name}](https://github.com/PinkR1ver/JudeW-Problemset/tree/master/Leetcode/" + dir_name.replace(' ', '%20') + ")")
+        
+    # Restore the standard output
+    sys.stdout = original_stdout
         
